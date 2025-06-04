4a012a9b5951d9870bd7aacda9090e9bb2c0262d
Renée Kooi <renee.kooi@apollographql.com>
2025-06-04 10:04:59 +0200

    Remove excess dbg!

--- apollo-router/tests/common.rs
+++ apollo-router/tests/common.rs
@@ -537,7 +537,7 @@ impl IntegrationTest {
                 .env("APOLLO_GRAPH_REF", apollo_graph_ref);
         }
         router
-            .args(dbg!([
+            .args([
                 "--hr",
                 "--config",
                 &self.test_config_location.to_string_lossy(),
@@ -545,7 +545,7 @@ impl IntegrationTest {
                 &self.test_schema_location.to_string_lossy(),
                 "--log",
                 &self.log,
-            ]))
+            ])
             .stdout(Stdio::piped());
 
         let mut router = router.spawn().expect("router should start");
